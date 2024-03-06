089a6b73134489ba4ef203d9bc1a147f87701c4b
ArthurCloud <arthursarkisian@yahoo.com>
2024-03-06 23:11:38 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 jQuery(document).ready(function ($) {
     $('#comment-form').submit(function () {
         var formData = $(this).serialize();
+        var postID = $(this).data('post-id');
+        var nonce = $('#comment_nonce').val();
+
         $.ajax({
             type: 'POST',
-            url: ajaxurl,
-            data: formData + '&action=add_comment', 
+            url: custom_ajax_obj.ajaxurl,
+            data: formData + '&action=add_comment&post_id=' + postID + '&comment_nonce=' + nonce,
             success: function (response) {
                 $('#comments-list').prepend(response);
-                $('#comment-form')[0].reset(); 
+                $('#comment-form')[0].reset();
             }
         });
+
         return false;
     });
 });
